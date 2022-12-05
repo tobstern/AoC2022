8b28af14242ef0aa2556500f45ce9c6c500b054d
tobstern <tobiasphilipp11@gmail.com>
2022-12-05 13:40:48 +0100

    finished day05 part 2

--- day05.py
+++ day05.py
@@ -1,9 +1,10 @@
 from time import perf_counter as pfc
 import numpy as np
 from collections import defaultdict as dd
+from copy import deepcopy
 
 day = "05"
-test = 1
+test = 0
 if test:
     t = "test_"
 else:
@@ -22,17 +23,16 @@ crates, instr = i[0], i[1]
 crates = np.array([list(c) for c in crates])
 # clean it up
 crates[(crates == " ") | (crates == "[") | (crates == "]")] = ""
-# print(crates)
+
 # get the column numbers of the crates
 cols = [j for j, col in enumerate(crates[-1]) if col != ""]
-# print(cols)
+
 # rearrange and filter them out
 crates = crates[:-1, cols].T
 crates_ini = dd(list)
 # create dictionary, bacause list are not equal in their length
 for j, stack in enumerate(crates):
     crates_ini[j + 1] = list(reversed(list(stack[stack != ""])))
-# print(crates_ini)
 
 # edit input - instructions (instr) -
 instr = [
@@ -49,11 +49,10 @@ instr = [
     ]
     for ins in instr
 ]
-# print(instr)
 
 # Part 1:
 start1 = pfc()
-crates1 = crates_ini.copy()
+crates1 = deepcopy(crates_ini)
 for j, l in enumerate(instr):
     # move x times a crate from start to aim
     x, start, aim = l[0], l[1][0], l[1][1]
@@ -61,10 +60,20 @@ for j, l in enumerate(instr):
     # transferred
     crates1[aim] += list(reversed(crates1[start][-x:]))
     crates1[start] = crates1[start][:-x]
-# print(crates1)
+
 message1 = "".join([vals[-1] for vals in crates1.values()])
 print(f"Part 1 result is: {message1}, t = {pfc() - start1}")
 
 # Part 2:
 start2 = pfc()
-print(f"Part 2 result is: {day}, t = {pfc() - start2}")
+crates2 = deepcopy(crates_ini)
+for j, l in enumerate(instr):
+    # move x times a crate from start to aim
+    x, start, aim = l[0], l[1][0], l[1][1]
+    # before adding, reverse the list of crates that are to be
+    # transferred
+    crates2[aim] += crates2[start][-x:]
+    crates2[start] = crates2[start][:-x]
+
+message2 = "".join([vals[-1] for vals in crates2.values()])
+print(f"Part 2 result is: {message2}, t = {pfc() - start2}")
