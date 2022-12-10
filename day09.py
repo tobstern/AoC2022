026e2d99ae20c31c9aec3ac13486e5a4113bcd9a
tobstern <tobiasphilipp11@gmail.com>
2022-12-10 23:03:28 +0100

    finally had time to finish part 2

--- day09.py
+++ day09.py
@@ -2,7 +2,7 @@ from time import perf_counter as pfc
 from collections import defaultdict as dd
 
 day = "09"
-test = 1
+test = 0
 if test:
     # t = "test_"
     t = "test2_"
@@ -18,11 +18,14 @@ moves = [
 ]
 
 
-def HTdist(Hpos, Tpos):
+def HTdist(prev_pos, Tpos):
     return list(
         map(
             abs,
-            [Tpos[-1][0] - Hpos[-1][0], Tpos[-1][1] - Hpos[-1][1]],
+            [
+                Tpos[-1][0] - prev_pos[-1][0],
+                Tpos[-1][1] - prev_pos[-1][1],
+            ],
         )
     )
 
@@ -84,53 +87,64 @@ visited_pos = len(set(Tpos))
 print(f"Part 1 result is: {visited_pos}, t = {pfc() - start1}")
 
 
-def moveT2(Hpos, Tposes):
+def moveT2(prev_pos, Tposes, T):
     # move the Tail
     # it follows its leading knot, respectively
-    # find direction and only move by one (build unit vector)
-    Lpos = Hpos.copy()  # Lpos is Leading position
-    for T in range(1, 10):
-        Tpos = Tposes[T].copy()
-        if T > 1:
-            Lpos = Tposes[T - 1].copy()
-        HT_dist = HTdist(Lpos, Tpos)
-        if HT_dist[0] == 2 or HT_dist[1] == 2:
-            vec = [
-                int(
-                    (Lpos[-1][p] - Tpos[-1][p])
-                    / abs(Lpos[-1][p] - Tpos[-1][p])
-                )
-                if Lpos[-1][p] != Tpos[-1][p]
-                else 0
-                for p in [0, 1]
-            ]
-            Tposes[T] += [tuple(Tpos[-1][p] + vec[p] for p in [0, 1])]
-            # print(f"The updated Tpositions are: {Tpos}")
-        else:
-            return
-        print(f"Tposes dict is: {Tposes}")
+    # find direction and only move by one (build unit vector).
+    # check to break the recursion - for the Tails:
+    if T > 9:
+        # did it reach the end of the tail, yet?
+        return
+
+    Tpos = Tposes[T]
+    HT_dist = HTdist(prev_pos, Tpos)
+
+    # move to follow the leading knot of the Tail (if 2 steps away)
+    if max(HT_dist) == 2:
+        vec = [
+            int(
+                (prev_pos[-1][p] - Tpos[-1][p])
+                / abs(prev_pos[-1][p] - Tpos[-1][p])
+            )
+            if prev_pos[-1][p] != Tpos[-1][p]
+            else 0
+            for p in [0, 1]
+        ]
+        Tposes[T] += [tuple(Tpos[-1][p] + vec[p] for p in [0, 1])]
+        # move all following tails (that need to move)
+        moveT2(Tposes[T], Tposes, T + 1)
+    else:
+        return
 
 
-def sim_rope2(d, steps, Hpos, Tpos):
+def sim_rope2(d, steps, Hpos, Tposes):
     # takes the direction (d) and the steps count
     # left, right, up, down
     for s in range(1, steps + 1):
         # go left
         if d == "L":
             Hpos += [(Hpos[-1][0], Hpos[-1][1] - 1)]
-            moveT2(Hpos, Tposes)
+            HT_dist = HTdist(Hpos, Tposes[1])
+            if HT_dist[1] == 2:
+                moveT2(Hpos, Tposes, 1)
         # go right
         if d == "R":
             Hpos += [(Hpos[-1][0], Hpos[-1][1] + 1)]
-            moveT2(Hpos, Tposes)
+            HT_dist = HTdist(Hpos, Tposes[1])
+            if HT_dist[1] == 2:
+                moveT2(Hpos, Tposes, 1)
         # go up
         if d == "U":
             Hpos += [(Hpos[-1][0] + 1, Hpos[-1][1])]
-            moveT2(Hpos, Tposes)
+            HT_dist = HTdist(Hpos, Tposes[1])
+            if HT_dist[0] == 2:
+                moveT2(Hpos, Tposes, 1)
         # go down
         if d == "D":
             Hpos += [(Hpos[-1][0] - 1, Hpos[-1][1])]
-            moveT2(Hpos, Tposes)
+            HT_dist = HTdist(Hpos, Tposes[1])
+            if HT_dist[0] == 2:
+                moveT2(Hpos, Tposes, 1)
 
 
 # Part 2:
@@ -141,20 +155,11 @@ Hpos = [(0, 0)]  # Head positions
 Tpos = [(0, 0)]  # Tail positions
 Tposes = dd(list)
 for T in range(1, 10):
-    Tposes[T] = Tpos
+    Tposes[T] = Tpos.copy()
 #
-print(Tposes)
 for d, c in moves:
     sim_rope2(d, c, Hpos, Tposes)
 
-# find postions, that are visited by all Tails
-visited_pos2 = 0
-for pos in Tposes[1]:
-    print(pos)
-    print([1 if pos in Tposes[T] else 0 for T in range(1, 10)])
-    l = [1 if pos in Tposes[T] else 0 for T in range(1, 10)]
-    if sum(l) == len(l):
-        # then the postion has been visited by all knots
-        visited_pos2 += 1
-
+# find postions, that are visited by the Tails (look at position 9)
+visited_pos2 = len(set(Tposes[9]))
 print(f"Part 2 result is: {visited_pos2}, t = {pfc() - start2}")
