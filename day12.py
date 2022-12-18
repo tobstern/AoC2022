d586a0b97c0e9a0cffe03819a8a115ceca4629a7
tobstern <tobiasphilipp11@gmail.com>
2022-12-18 16:08:40 +0100

    still in progress (only beginning of route is wrong)

--- day12.py
+++ day12.py
@@ -1,5 +1,10 @@
 from time import perf_counter as pfc
 import numpy as np
+from collections import defaultdict as dd
+import sys
+
+print(sys.getrecursionlimit())
+sys.setrecursionlimit(10000)
 
 day = "12"
 test = 1
@@ -17,37 +22,39 @@ def o(li):
     Returns list with integers.
     """
     return [
-        ord(char) - 96
+        ord(char) - 97
         if char not in ["S", "E"]
-        else 0
+        else "S"
         if char == "S"
-        else 27
+        else "E"
         for char in li
     ]
 
 
-hmap = np.array(
-    [
-        o(list(l))
-        for l in open(
-            "./puzzle_inputs/" + t + "day" + day + ".txt", "r"
-        )
-        .read()
-        .strip()
-        .splitlines()
-    ]
-)
+hmap = [
+    o(list(l))
+    for l in open("./puzzle_inputs/" + t + "day" + day + ".txt", "r")
+    .read()
+    .strip()
+    .splitlines()
+]
+
+
+def add_key(d, key, val):
+    if key in d.keys():
+        d[key].update(val)
+    else:
+        d[key] = val
 
 
 def create_graph():
     """
     Generates graph.
     """
-    c = 0
-    for l in pos_map:
-        for (i, j) in l:
+    for i in range(height):
+        for j in range(width):
             # position can have all 4 neighbours
-            current = (i, j)
+            cur = (i, j)
             left, right, up, down = (
                 (i, j - 1),
                 (i, j + 1),
@@ -55,50 +62,47 @@ def create_graph():
                 (i + 1, j),
             )
             # test if postion exists:
-            if left not in flat_pos_map:
-                left = []
-            if right not in flat_pos_map:
-                right = []
-            if up not in flat_pos_map:
-                up = []
-            if down not in flat_pos_map:
-                down = []
-            neighbours_elevation = [
-                {
-                    left[0] * width
-                    + left[1]: abs(hmap[left] - hmap[current]),
-                }
-                if left != []
-                else None,
-                {
-                    right[0] * width
-                    + right[1]: abs(hmap[right] - hmap[current]),
-                }
-                if right != []
-                else None,
-                {up[0] * width + up[1]: abs(hmap[up] - hmap[current])}
-                if up != []
-                else None,
-                {
-                    down[0] * width
-                    + down[1]: abs(hmap[down] - hmap[current]),
-                }
-                if down != []
-                else None,
-            ]
-            graph[c] = {
-                k: v
-                for ele in neighbours_elevation
-                if ele != None
-                for k, v in ele.items()
-            }
-            c += 1
+            if left in flat_pos_map:
+                add_key(
+                    graph,
+                    cur,
+                    {left: abs(hmap[left[0]][left[1]] - hmap[i][j])},
+                )
+            if right in flat_pos_map:
+                add_key(
+                    graph,
+                    cur,
+                    {right: abs(hmap[right[0]][right[1]] - hmap[i][j])},
+                )
+            if up in flat_pos_map:
+                add_key(
+                    graph,
+                    cur,
+                    {up: abs(hmap[up[0]][up[1]] - hmap[i][j])},
+                )
+            if down in flat_pos_map:
+                add_key(
+                    graph,
+                    cur,
+                    {down: abs(hmap[down[0]][down[1]] - hmap[i][j])},
+                )
+
+
+def get_path(adj_nodes, node, c):
+    # recursively call adjacent nodes till reaching aim:
+    # return count
+    print(adj_nodes[node])
+    if adj_nodes[node] == None:
+        return c
+    c += 1
+    get_path(adj_nodes, adj_nodes[node], c)
 
 
 def dijkstras(graph, root, dest):
     # (i, j) => node = i * width + j
-    paths = [float("inf")] * (width * height)
-    # visited = [False] * (width * height)
+    c = 0  # the count of passed nodes
+    paths = {pos: float("inf") for pos in graph.keys()}
+    seen = []
     adj_nodes = {i: None for i in list(graph.keys())}
     queue = list(graph.keys())
     # set root elevation to 0
@@ -106,40 +110,47 @@ def dijkstras(graph, root, dest):
 
     # loop through queue with all nodes:
     while queue:
-        lowest_n = queue[root]
+        lowest_n = queue[0]
         min_val = paths[lowest_n]
         # loop through all elements in queue
         for n in range(1, len(queue)):
-            if (
-                paths[queue[n]] <= min_val
-                or paths[queue[n]] == min_val + 1
-            ):
+            if paths[queue[n]] <= min_val + 1:
                 lowest_n = queue[n]
                 min_val = paths[lowest_n]
         cur = lowest_n
         queue.remove(cur)
-        print(cur)
+        # print(cur)
 
         # now loop through the neighbours and alternate routes:
+        # if cur == 27:
+        #     break
         for neigh in graph[cur]:
-            print("Current node:", cur)
-            print("Neighbour:", neigh)
-            print("Graph @ cur", graph[cur])
-            alternate = graph[cur][neigh] + paths[cur]
-            if paths[neigh] > alternate and paths[neigh]:
-                paths[neigh] = alternate
-                adj_nodes[neigh] = cur
+            # print("Current node:", cur)
+            # print("Neighbour:", neigh)
+            # print("Graph @ cur", graph[cur])
+            alternate = paths[cur] + graph[cur][neigh]
+            if paths[neigh] > alternate:
+                print(paths[neigh])
+                if graph[cur][neigh] <= 1:
+                    paths[neigh] = alternate
+                    adj_nodes[neigh] = cur
+                    # c += 1 if cur not in seen else 0
+                    # seen += [cur]
 
-    # show the path and count the steps
-    x = dest
-    c = 0  # the count of passed nodes
-    while True:
-        x = adj_nodes[x]
-        if x == None:
-            print("-Arrived at the top-")
-            break
-        print(x, end="<-")
-        c += 1
+    # print(len([v for v in adj_nodes.values() if v != None]))
+    # # show the path and count the steps
+    # x = dest
+    # c = 0  # the count of passed nodes
+    # # print("-Started at the top-")
+    # while True:
+    #     x = adj_nodes[x]
+    #     if x == None:
+    #         # print("\n-Arrived at the bottom-")
+    #         break
+    #     # print(x, end="<-")
+    #     c += 1 if x != (0, 0) else -1
+    print(adj_nodes)
+    return get_path(adj_nodes, dest, 0)
 
 
 # Part 1:
@@ -148,31 +159,43 @@ start1 = pfc()
 # find shortest path from S to E! - use Dijkstra's algorithm –
 # note: you can only step on one level higher,
 # but multiple level down?
-graph = {}
-height, width = hmap.shape
+graph = dd(dict)
+height, width = len(hmap), len(hmap[0])
 # create position tuples for the neighbour search
-pos_map = []
-for i in range(height):
-    help = []
-    for j in range(width):
-        help += [(i, j)]
-    pos_map += [help]
 # print(width, height)
-print(pos_map)
-print(hmap)
-flat_pos_map = [pos for l in pos_map for pos in l]
-flat_hmap = [h for l in hmap for h in l]
-root = list(flat_hmap).index(0)
-dest = list(flat_hmap).index(27)
+# print(pos_map)
+# print(hmap)
+flat_pos_map = [(i, j) for i in range(height) for j in range(width)]
+root = [
+    (i, j)
+    for i in range(height)
+    for j in range(width)
+    if hmap[i][j] == "S"
+][0]
+dest = [
+    (i, j)
+    for i in range(height)
+    for j in range(width)
+    if hmap[i][j] == "E"
+][0]
+hmap[root[0]][root[1]] = 0
+hmap[dest[0]][dest[1]] = 25
+print(root, dest)
 
 create_graph()
 print(graph)
+create_graph_time = pfc()
 
 # through all nodes (position as tuples( ))
-# (i, j) => node = i * width + j
-dijkstras(graph, root, dest)
+c = dijkstras(graph, root, dest)
+dijkstras_time = pfc()
 
-print(f"Part 1 result is: {day}, t = {pfc() - start1}")
+print(
+    f"Part 1 result is: {c}, whole_t = {pfc() - start1}, "
+    + f"graph_creation_time = {pfc() - create_graph_time}, "
+    + f"process_dijkstra_time = {pfc() - dijkstras_time}"
+)
+# 1145 is too high
 
 # Part 2:
 start2 = pfc()
