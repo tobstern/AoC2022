e7aee9e6d668efad8e1b5ec669295f56eaf1e626
tobstern <tobiasphilipp11@gmail.com>
2023-01-04 09:47:33 +0100

    day23 part 1 and 2

--- day23.py
+++ day23.py
@@ -1,7 +1,4 @@
 from time import perf_counter as pfc
-from collections import deque
-from collections import Counter as C
-from copy import deepcopy as dc
 
 day = "23"
 test = 0
@@ -10,77 +7,29 @@ if test:
 else:
     t = ""
 #
-elves = {}
-c = -1
+elves = set()
 for i, l in enumerate(open("./puzzle_inputs/" + t + "day" + day + ".txt", "r")):
     # print(l)
     for j, ele in enumerate(l):
         if ele == "#":
-            c += 1
             # is an elve, save its position:
-            elves[c] = (i, j)
+            elves.add((i, j))
 
 # print(elves)
-
-
-def consider(c, elf):
-    y, x = elf
-
-    seen = []
-
-    proposal = []
-
-    for dy, dx in adjacents:
-        # check all 8 adj:
-        if (y + dy, x + dx) not in elves.values():
-            # no other elf in any direction -> do nothing
-            continue
-        seen.append((y + dy, x + dx))
-
-    if len(seen) < 1:
-        return proposal
-
-    # look in all directions (in the order of dirs)
-    for d in first_valid[c]:
-        # print(any([(y + dy, x + dx) in seen for dy, dx in dir_dict[d]]))
-        if all([(y + dy, x + dx) not in seen for dy, dx in dir_dict[d]]):
-            # there is no elf in these 3 directions
-            if d == "N":
-                proposal = (y - 1, x)
-            if d == "S":
-                proposal = (y + 1, x)
-            if d == "W":
-                proposal = (y, x - 1)
-            if d == "E":
-                proposal = (y, x + 1)
-
-            # save the considered direction for this elf
-            # print(first_valid[c])
-            dirs = first_valid[c]
-            # print(dirs)
-            dirs.remove(d)
-            first_valid[c].append(d)
-            # print(first_valid[c])
-            break
-    # now either there is a proposed direction of that elf
-    # or it will not move
-    # print("proposal", proposal)
-    return proposal
-
 
 # Part 1:
 start1 = pfc()
 
-rounds = 10
 dir_dict = {
-    "N": [(-1, 0), (-1, 1), (-1, -1)],
-    "S": [(1, 0), (1, 1), (1, -1)],
-    "W": [(0, -1), (-1, -1), (1, -1)],
-    "E": [(0, 1), (-1, 1), (1, 1)],
+    (-1, 0): [(-1, 0), (-1, 1), (-1, -1)],
+    (1, 0): [(1, 0), (1, 1), (1, -1)],
+    (0, -1): [(0, -1), (-1, -1), (1, -1)],
+    (0, 1): [(0, 1), (-1, 1), (1, 1)],
 }
 # dirs = deque(["N", "S", "W", "E"])
-first_valid = {i: deque(["N", "S", "W", "E"]) for i, elf in enumerate(elves)}
-# dirs = deque([(-1, 0), (1, 0), (0, -1), (0, 1)])
+# dirs = {i: deque(["N", "S", "W", "E"]) for i, elf in enumerate(elves)}
+# dirs = deque(["N", "S", "W", "E"])
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 # adjacents = ["N", "NE", "NW", "S", "SE", "SW", "W", "E"]
 adjacents = [
     (-1, 0),
@@ -94,58 +43,148 @@ adjacents = [
 ]
 
 
-while rounds:
+for _ in range(10):
     # consider the move and then move
-    propose = {}
+    propose = set()
+    propose_double = set()
 
-    for i, elf in elves.items():
-        proposal = consider(i, elf)
-        propose[i] = proposal
-        # print(propose)
+    for elf in elves:
+        y, x = elf
+        if all((y + dy, x + dx) not in elves for dy, dx in adjacents):
+            # has no neighbours -> skip elf
+            continue
+
+        # look in all directions (in the order of dirs)
+        for d in dirs:
+            if all((y + dy, x + dx) not in elves for dy, dx in dir_dict[d]):
+                dy, dx = d
+                proposal = (y + dy, x + dx)
+                # there is no elf in these 3 directions
+                # save the considered direction for this elf
+                if proposal in propose_double:
+                    pass
+                elif proposal in propose:
+                    # doublicate
+                    propose_double.add(proposal)
+                else:
+                    # consider(elf)
+                    propose.add(proposal)
+                break
 
     # make the moves
-    # propose_ = dc(propose)
-    propose = {
-        i: pos
-        for i, pos in propose.items()
-        if list(propose.values()).count(pos) < 2
-    }
-    # print(occ)
+    elves_c = set(elves)
 
-    # print(propose_)
-    for i, pos in propose.items():
-        # print(i, pos)
-        elves[i] = pos
-        # if pos == []:
-        #    continue
+    for elf in elves_c:
+        y, x = elf
+        if all((y + dy, x + dx) not in elves_c for dy, dx in adjacents):
+            # has no neighbours -> skip elf
+            continue
 
-        # if occ[i] < 2:
-        # there are no other elves considering this direction
-        # next_elves.append(considered)
-        #    elves[i] = pos
+        for d in dirs:
+            # if in direction d and its diagonals is no elf
+            if all((y + dy, x + dx) not in elves_c for dy, dx in dir_dict[d]):
+                # move the elf
+                dy, dx = d
+                next_move = (y + dy, x + dx)
+                if next_move not in propose_double:
+                    # move the elve to the next position
+                    elves.remove(elf)
+                    elves.add(next_move)
+                break
 
-    rounds -= 1
+    dirs.append(dirs.pop(0))
 
-# print(elves)
-
-# print the empty ground tiles -> every point in the square - len(elves)
+# min and max values for the bounding box
 my, mx = float("inf"), float("inf")
 
 My, Mx = -float("inf"), -float("inf")
 
-for y, x in elves.values():
+for y, x in elves:
     my, mx = min(my, y), min(mx, x)
 
     My, Mx = max(My, y), max(Mx, x)
 
-print(My, my, Mx, mx, len(elves.keys()))
-empty = (My - my + 1) * (Mx - mx + 1)
-# empty = (My - my + 1) * (Mx - mx + 1) - len(elves.keys())
-
+# number of empty tiles
+empty = (My - my + 1) * (Mx - mx + 1) - len(elves)
 
 print(f"Part 1 result is: {empty}, t = {pfc() - start1}")
-# 3389 too low, 5776 too high,    3544?,
 
 # Part 2:
 start2 = pfc()
-print(f"Part 2 result is: {day}, t = {pfc() - start2}")
+
+elves = set()
+for i, l in enumerate(open("./puzzle_inputs/" + t + "day" + day + ".txt", "r")):
+    # print(l)
+    for j, ele in enumerate(l):
+        if ele == "#":
+            # is an elve, save its position:
+            elves.add((i, j))
+
+
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+rounds = 0
+
+last_elves = set(elves)
+
+while True:
+    # consider the move and then move
+    propose = set()
+    propose_double = set()
+
+    for elf in elves:
+        y, x = elf
+        if all((y + dy, x + dx) not in elves for dy, dx in adjacents):
+            # has no neighbours -> skip elf
+            continue
+
+        # look in all directions (in the order of dirs)
+        for d in dirs:
+            if all((y + dy, x + dx) not in elves for dy, dx in dir_dict[d]):
+                dy, dx = d
+                proposal = (y + dy, x + dx)
+                # there is no elf in these 3 directions
+                # save the considered direction for this elf
+                if proposal in propose_double:
+                    pass
+                elif proposal in propose:
+                    # doublicate
+                    propose_double.add(proposal)
+                else:
+                    # consider(elf)
+                    propose.add(proposal)
+                break
+
+    # make the moves
+    elves_c = set(elves)
+
+    for elf in elves_c:
+        y, x = elf
+        if all((y + dy, x + dx) not in elves_c for dy, dx in adjacents):
+            # has no neighbours -> skip elf
+            continue
+
+        for d in dirs:
+            # if in direction d and its diagonals is no elf
+            if all((y + dy, x + dx) not in elves_c for dy, dx in dir_dict[d]):
+                # move the elf
+                dy, dx = d
+                next_move = (y + dy, x + dx)
+                if next_move not in propose_double:
+                    # move the elve to the next position
+                    elves.remove(elf)
+                    elves.add(next_move)
+                break
+
+    dirs.append(dirs.pop(0))
+
+    rounds += 1
+
+    # the previous and the current sets are equal -> no further movement
+    if last_elves == elves:
+        break
+
+    last_elves = set(elves)
+
+
+print(f"Part 2 result is: {rounds}, t = {pfc() - start2}")
