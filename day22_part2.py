part 2 still WIP (fixed rotation)

day22_part2.py
@@ -43,34 +43,64 @@ for i, line in enumerate(grid):
 
 
 # print([len(fd[i]) for i in range(1, 7)])
+# f = 5
+# print(np.array(fd[f])[-1, :], np.array(fd[f]).shape)
 # exit()
 
 
 def is_wall(next_pos, face):
     # check if wall is next
-    if fd[face][next_pos[0]][next_pos[1]] == "#":
+    y, x = next_pos
+    if fd[face][y][x] == "#":
         return True
     else:
         return False
 
 
-def get_pos(next_dir, cur_pos):
+def get_pos(cur_dir, next_dir, cur_pos):
     y, x = cur_pos
 
+    # return y, x
+    cd = 4 if cur_dir == 0 else cur_dir
+    nd = 4 if next_dir == 0 else next_dir
+
+    # θ = -(nd - cd) * pi / 2
+    θ = -(nd - cd) * 90
+
+    # rotate coordinates
+    if θ == 0:
+        sinθ, cosθ = 0, 1
+        # do not change y,x
+        yd, xd = y, x
+
+    elif θ == 90 or θ == -270:
+        sinθ, cosθ = 1, 0
+        yd, xd = x, y % cube_max
+
+    elif θ == -90 or θ == 270:
+        sinθ, cosθ = -1, 0
+        yd, xd = x % cube_max, y
+
+    elif θ == 180 or θ == -180:
+        sinθ, cosθ = 0, -1
+        yd, xd = (cube_max - y - 1) % cube_max, (cube_max - x - 1) % cube_max
+
+    # assign depending on next direction
     if next_dir == 0:
-        return y, 0
+        return yd, 0
 
     if next_dir == 1:
-        return 0, x
+        return 0, xd
 
     if next_dir == 2:
-        return y, cube_max - 1
+        return yd, cube_max - 1
 
     if next_dir == 3:
-        return cube_max - 1, x
+        return cube_max - 1, xd
 
 
 # Part 2:
+lim = 82
 start2 = pfc()
 # same rules, but grid changes to cube
 
@@ -109,78 +139,148 @@ corners = {
 # -> wrap to next_face, next_dir
 # at end of instructions -> get "real" position of original grid
 
-for i in instructions:
+for c, i in enumerate(instructions):
     # start at (top left -> get pos after empty space), facing right
+    hit = False
+
+    print("\n\n# --", c, "-- #")
+
     if type(i) == str:
+        print("\nfrom cur_dir:", cur_dir)
         # change direction by "90°" - select next direction in directions list
         cur_dir = directions[(cur_dir + 1 if i == "R" else cur_dir - 1) % 4]
+        print("rotate to:", cur_dir)
 
+    # elif type(i) == int:
     else:
         # type is int -> steps to go:
-        hit = False
+
         ic = i
 
-        while not hit:
+        print(
+            "\nNext instruction:",
+            "cur_pos:",
+            cur_pos,
+            "steps to make:",
+            ic,
+            "cur_dir:",
+            cur_dir,
+            "cur_face:",
+            cur_face,
+            "\n",
+        )
+        while ic and not hit:
             y, x = cur_pos
 
-            # if ic <= 0:
-            #    break
+            print(
+                "Start the stepping:",
+                "cur_pos:",
+                cur_pos,
+                "steps to make:",
+                ic,
+                "cur_dir:",
+                cur_dir,
+                "cur_face:",
+                cur_face,
+            )
+
+            next_face, next_dir = cube[cur_face][cur_dir]
+            # print("cur_dir:", cur_dir, "next_dir:", next_dir)
+
+            # offset for the 'real' coordinates
+            oy, ox = corners[cur_face]
+            noy, nox = corners[next_face]
 
             if cur_dir == 0:
                 # +x, right
+
                 for j in range(ic):
 
                     if x + 1 >= cube_max:
                         # hit the boundary:
-                        next_face, next_dir = cube[cur_face][cur_dir]
 
                         yc, xc = y, x  # old position
-                        if abs(cur_dir - next_dir) % 2 != 0:
-                            # swap the coordinates (y, x)
-                            temp_ = y
-                            y = x
-                            x = temp_
-                        y_, x_ = get_pos(next_dir, (y, x))
+                        y_, x_ = get_pos(cur_dir, next_dir, (y, x))
+
+                        print(
+                            "bound - right (0):",
+                            "steps made:",
+                            j,
+                            "before:",
+                            (yc + oy, xc + ox),
+                            "after:",
+                            (y_ + noy, x_ + nox),
+                            "next dir:",
+                            next_dir,
+                            "cur face:",
+                            cur_face,
+                            "next face:",
+                            next_face,
+                        )
 
                         if is_wall((y_, x_), next_face):
-                            cur_pos = (yc, xc)
+                            cur_pos = (y, x) = (yc, xc)
                             hit = True
                             break
                         else:
                             cur_face = next_face
                             cur_dir = next_dir
-                            cur_pos = (y_, x_)
-                            ic -= j
+                            y, x = y_, x_
+                            cur_pos = (y, x)
+                            ic = ic - j - 1
+                            print(
+                                "move to next face:",
+                                cur_face,
+                                "next_pos:",
+                                cur_pos,
+                                "next steps to make:",
+                                ic,
+                                "next_dir",
+                                cur_dir,
+                            )
                             break
 
                     if is_wall((y, x + 1), cur_face):
                         # stop @ previous
                         cur_pos = (y, x)
                         hit = True
+                        print(
+                            "Normal stepping - hit wall - made:",
+                            j,
+                            "steps, ",
+                            "@",
+                            cur_pos,
+                        )
                         break
 
                     x += 1
 
-                else:
-                    # steps are done - next instruction!
-                    cur_pos = (y, x)
-                    break
-
             elif cur_dir == 1:
                 # +y, down
+
                 for j in range(ic):
 
                     if y + 1 >= cube_max:
                         # hit the boundary:
-                        next_face, next_dir = cube[cur_face][cur_dir]
 
                         yc, xc = y, x  # old position
-                        if abs(cur_dir - next_dir) % 2 != 0:
-                            # swap the coordinates (y, x)
-                            temp_ = y
-                            y = x
-                            x = temp_
-                        y_, x_ = get_pos(next_dir, (y, x))
+                        y_, x_ = get_pos(cur_dir, next_dir, (y, x))
+
+                        print(
+                            "bound - down (1):",
+                            "steps made:",
+                            j,
+                            "before:",
+                            (yc + oy, xc + ox),
+                            "after:",
+                            (y_ + noy, x_ + nox),
+                            "next dir:",
+                            next_dir,
+                            "cur face:",
+                            cur_face,
+                            "next face:",
+                            next_face,
+                        )
 
                         if is_wall((y_, x_), next_face):
                             cur_pos = (yc, xc)
@@ -189,38 +289,62 @@ for i in instructions:
                         else:
                             cur_face = next_face
                             cur_dir = next_dir
-                            cur_pos = (y_, x_)
-                            ic -= j
+                            y, x = y_, x_
+                            cur_pos = (y, x)
+                            ic = ic - j - 1
+                            print(
+                                "move to next face:",
+                                cur_face,
+                                "next_pos:",
+                                cur_pos,
+                                "next steps to make:",
+                                ic,
+                                "next_dir",
+                                cur_dir,
+                            )
                             break
 
                     if is_wall((y + 1, x), cur_face):
                         # stop @ previous
                         cur_pos = (y, x)
                         hit = True
+                        print(
+                            "Normal stepping - hit wall - made:",
+                            j,
+                            "steps, ",
+                            "@",
+                            cur_pos,
+                        )
                         break
 
                     y += 1
 
-                else:
-                    # steps are done - next instruction!
-                    cur_pos = (y, x)
-                    break
-
             elif cur_dir == 2:
                 # -x, left
+
                 for j in range(ic):
 
                     if x - 1 < 0:
                         # hit the boundary:
-                        next_face, next_dir = cube[cur_face][cur_dir]
 
                         yc, xc = y, x  # old position
-                        if abs(cur_dir - next_dir) % 2 != 0:
-                            # swap the coordinates (y, x)
-                            temp_ = y
-                            y = x
-                            x = temp_
-                        y_, x_ = get_pos(next_dir, (y, x))
+                        y_, x_ = get_pos(cur_dir, next_dir, (y, x))
+
+                        print(
+                            "bound - left (2):",
+                            "steps made:",
+                            j,
+                            "before:",
+                            (yc + oy, xc + ox),
+                            "after:",
+                            (y_ + noy, x_ + nox),
+                            "next dir:",
+                            next_dir,
+                            "cur face:",
+                            cur_face,
+                            "next face:",
+                            next_face,
+                        )
 
                         if is_wall((y_, x_), next_face):
                             cur_pos = (yc, xc)
@@ -229,38 +353,62 @@ for i in instructions:
                         else:
                             cur_face = next_face
                             cur_dir = next_dir
-                            cur_pos = (y_, x_)
-                            ic -= j
+                            y, x = y_, x_
+                            cur_pos = (y, x)
+                            ic = ic - j - 1
+                            print(
+                                "move to next face:",
+                                cur_face,
+                                "next_pos:",
+                                cur_pos,
+                                "next steps to make:",
+                                ic,
+                                "next_dir",
+                                cur_dir,
+                            )
                             break
 
                     if is_wall((y, x - 1), cur_face):
                         # stop @ previous
                         cur_pos = (y, x)
                         hit = True
+                        print(
+                            "Normal stepping - hit wall - made:",
+                            j,
+                            "steps, ",
+                            "@",
+                            cur_pos,
+                        )
                         break
 
                     x -= 1
 
-                else:
-                    # steps are done - next instruction!
-                    cur_pos = (y, x)
-                    break
-
             elif cur_dir == 3:
                 # -y, up
+
                 for j in range(ic):
 
                     if y - 1 < 0:
                         # hit the boundary:
-                        next_face, next_dir = cube[cur_face][cur_dir]
 
                         yc, xc = y, x  # old position
-                        if abs(cur_dir - next_dir) % 2 != 0:
-                            # swap the coordinates (y, x)
-                            temp_ = y
-                            y = x
-                            x = temp_
-                        y_, x_ = get_pos(next_dir, (y, x))
+                        y_, x_ = get_pos(cur_dir, next_dir, (y, x))
+
+                        print(
+                            "bound - up (3):",
+                            "steps made:",
+                            j,
+                            "before:",
+                            (yc + oy, xc + ox),
+                            "after:",
+                            (y_ + noy, x_ + nox),
+                            "next dir:",
+                            next_dir,
+                            "cur face:",
+                            cur_face,
+                            "next face:",
+                            next_face,
+                        )
 
                         if is_wall((y_, x_), next_face):
                             cur_pos = (yc, xc)
@@ -269,32 +417,64 @@ for i in instructions:
                         else:
                             cur_face = next_face
                             cur_dir = next_dir
-                            cur_pos = (y_, x_)
-                            ic -= j
+                            y, x = y_, x_
+                            cur_pos = (y, x)
+                            ic = ic - j - 1
+                            print(
+                                "move to next face:",
+                                cur_face,
+                                "next_pos:",
+                                cur_pos,
+                                "next steps to make:",
+                                ic,
+                                "next_dir",
+                                cur_dir,
+                            )
                             break
 
                     if is_wall((y - 1, x), cur_face):
                         # stop @ previous
                         cur_pos = (y, x)
                         hit = True
+                        print(
+                            "Normal stepping - hit wall - made:",
+                            j,
+                            "steps, ",
+                            "@",
+                            cur_pos,
+                        )
                         break
 
                     y -= 1
 
-                else:
-                    # steps are done - next instruction!
-                    cur_pos = (y, x)
-                    break
+            # steps are done - next instruction!
+            cur_pos = (y, x)
+            if not hit and (j == ic - 1):
+                print(
+                    "All",
+                    j + 1,
+                    "steps finished, ",
+                    "@",
+                    cur_pos,
+                )
+                break
 
-print("cur_face:", cur_face)
+            continue
+    if c == -1:
+        print(len(instructions))
+        # print(cube)
+        exit(0)
+
+print("\nlast_face:", cur_face)
 
 oy, ox = corners[cur_face]
 y, x = cur_pos
+print("last_pos:", cur_pos)
 
-cur_pos = oy + y, ox + x
+y, x = cur_pos = oy + y, ox + x
 
-print("direction:", cur_dir, "y-pos:", cur_pos[0] + 1, "x-pos:", cur_pos[1] + 1)
-res = sum([cur_dir, 1000 * (cur_pos[0] + 1), 4 * (cur_pos[1] + 1)])
+print("direction:", cur_dir, "y-pos:", y + 1, "x-pos:", x + 1)
+res = sum([cur_dir, 1000 * (y + 1), 4 * (x + 1)])
 
 print(f"Part 2 result is: {res}, t = {pfc() - start2}")
 # 110109 too high -> 107,76 = 108311